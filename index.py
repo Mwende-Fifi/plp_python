31e162caefbd1e82c45749b9763001b804aa5c8c
Mwende-Fifi <mwendekiindu@gmail.com>
2025-07-31 23:48:13 +0300

    Added index.py

---
+++ index.py
@@ -0,0 +1,28 @@
+basket = 10
+bucket = "water"
+my_name = "Faith - Angola"
+#print("This class is crazy!")
+#print(basket)
+
+#print("My bananas are:", basket)
+#print(my_name)
+
+jsDaddie = ['jsDaddie', 'jsDaddie', 'Mammie', 24]
+
+paulo = ('Software engineer', 16, 'UK')
+
+# Dictionary
+meseret = {
+    "name": "meseret",
+    "age": 25,
+    "country": "Ethiopia",
+    "fav_drink": ""
+
+}
+
+# Boolean Data type
+is_raining = False
+
+# Set Data type
+my_basket = {"Bananas", "Apples", "Mangoes", "Oranges"}
+my_basket
