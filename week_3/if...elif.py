8a1e9cf139dcf2f7f73dad3b22c103967808f6ec
Mwende-Fifi <mwendekiindu@gmail.com>
2025-08-18 17:48:03 +0300

    Add week 3 tutorial notes

---
+++ week_3/if...elif.py
@@ -0,0 +1,25 @@
+# temperature = 21
+
+# if temperature > 40:
+#     print("It's a hot day")
+# elif temperature > 20:
+#     print("It's a warm day")
+# elif temperature > 10:
+#     print("It's a cold day")
+# else:
+#     print("It's freezing cold")
+
+
+
+grade = 10
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
